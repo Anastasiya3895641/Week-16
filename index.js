e6dd128e6325f3f49ee681369b99f0326ccff51c
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-10-26 16:39:44 +0300

    Первое задание

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 let items = [];
 let a = document.querySelector("input");
 let sum = 0;
+errorMessage.innerHTML = "";
+result.innerHTML = "";
+result2.innerHTML = "";
 
 function sumInput() {
 
@@ -11,16 +14,24 @@ function sumInput() {
         items[i] = a[i].value;
 
 
-
-        if (value === "/^[A-Za-z]+$/") break;
-
+        if (value === "/^[A-Za-z]+$/") {
+            errorMessage.innerHTML =
+                `Вы вставили букву(-ы)<br>`;
+            break;
+        }
         items.push(+value);
     }
+
 
     for (let item of items) {
         sum += item;
     }
     return sum;
 }
+
+document.querySelector('errorMassage').innerHTML = html;
+document.querySelector('result').innerHTML = html;
+
+
 
 
